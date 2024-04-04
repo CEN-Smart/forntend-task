1bc95ab358f4c05331be6ae18c7a5fd5c4fffae5
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2024-04-04 16:38:18 +0100

    Update the fetch-products.ts file with the following code:

--- src/query/fetch-products.ts
+++ src/query/fetch-products.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { Product } from "./../types/product";
 
+const axiosInstance = axios.create({
+  baseURL: "https://fakestoreapi.com",
+});
+
 export const fetchProducts = async (): Promise<Product[]> => {
-  const { data } = await axios.get("https://fakestoreapi.com/products");
+  const { data } = await axiosInstance.get("/products");
   return data as Product[];
 };
 
@@ -14,8 +18,6 @@ export const fetchProduct = async ({
   queryKey,
 }: QueryKey): Promise<Product> => {
   const { productId } = queryKey[0];
-  const { data } = await axios.get(
-    `https://fakestoreapi.com/products/${productId}`
-  );
+  const { data } = await axiosInstance.get(`/products/${productId}`);
   return data as Product;
 };
