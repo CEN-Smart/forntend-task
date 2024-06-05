bbfa5bdf5da8efb6d6a2467ede9ccdff06878251
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2024-06-05 08:17:16 +0100

    Feat: Add / to the starting routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,32 @@ import CartPage from './pages/cart';
 import CheckOutPage from './pages/checkout';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <NavbarWrapper />,
-    errorElement: <div>404 Not Found</div>,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: 'cart',
-        element: <CartPage />,
-      },
-      {
-        path: 'cart/checkout',
-        element: <CheckOutPage />,
-      },
-      {
-        path: 'product/:productId',
-        element: <Product />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <NavbarWrapper />,
+		errorElement: <div>404 Not Found</div>,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/cart',
+				element: <CartPage />,
+			},
+			{
+				path: '/cart/checkout',
+				element: <CheckOutPage />,
+			},
+			{
+				path: '/product/:productId',
+				element: <Product />,
+			},
+		],
+	},
 ]);
 const App = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+	return <RouterProvider router={router} />;
 };
 
 export default App;
